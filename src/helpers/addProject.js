689d55c7464949a105d0c9eb12dc6b912280c1ea
Liav <oneandonlyliav@gmail.com>
2022-04-05 14:17:59 -0400

    added default new project name

--- src/helpers/addProject.js
+++ src/helpers/addProject.js
@@ -1,7 +1,7 @@
 import { toDoList } from './globals';
 import Project from '../factories/Project';
 
-export default function addProject() {
-  toDoList.push(new Project());
+export default function addProject(name) {
+  toDoList.push(new Project(name));
   console.log(toDoList);
 }
--- src/ui/projectAddOpen.js
+++ src/ui/projectAddOpen.js
@@ -3,7 +3,7 @@ import addProjectDisplay from './addProjectDisplay';
 import { toDoList } from '../helpers/globals';
 
 export default function projectAddOpen() {
-  addProject();
+  addProject('New Project');
   const { id } = toDoList[toDoList.length - 1];
-  addProjectDisplay('', '', id);
+  addProjectDisplay('New Project', '', id);
 }
