b115b68d9043bfc49edf92bb052644a7a5e83fa3
Liav <oneandonlyliav@gmail.com>
2022-04-06 14:06:54 -0400

    code for rendering both projects and tasks is now fully DRY

--- src/ui/renderSavedProjects.js
+++ src/ui/renderSavedProjects.js
@@ -1,8 +1,12 @@
 import { toDoList } from '../helpers/globals';
 import addProjectDisplay from './addProjectDisplay';
+import addTaskDisplay from './addTaskDisplay';
 
 export default function renderSavedProjects() {
-  toDoList.forEach((toDo) => {
-    addProjectDisplay(toDo.name, toDo.tasks, toDo.id);
+  toDoList.forEach((project) => {
+    addProjectDisplay(project.name, project.id);
+    project.tasks.forEach((task) => {
+      addTaskDisplay(project, task);
+    });
   });
 }
