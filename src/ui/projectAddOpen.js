93d7c5f54eafebca0edef88cd1684644b027e25a
Liav <oneandonlyliav@gmail.com>
2022-04-05 21:44:39 -0400

    all data now pulls properly from toDoList array

--- src/ui/projectAddOpen.js
+++ src/ui/projectAddOpen.js
@@ -4,6 +4,6 @@ import { toDoList } from '../helpers/globals';
 
 export default function projectAddOpen() {
   addProject('New Project');
-  const { id } = toDoList[toDoList.length - 1];
-  addProjectDisplay('New Project', '', id);
+  const projectIndex = toDoList[toDoList.length - 1];
+  addProjectDisplay(projectIndex.name, '', projectIndex.id);
 }
