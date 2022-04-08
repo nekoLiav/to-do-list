ce2af6dcade6bd34e4a0ac69a461ae323d0d31cc
Liav <oneandonlyliav@gmail.com>
2022-04-07 22:20:44 -0400

    fixed typos in function

--- src/ui/priorityColor.js
+++ src/ui/priorityColor.js
@@ -1,7 +1,7 @@
 export default function priorityColor(taskPriority, taskPriorityElement) {
   if (taskPriority === 'Low') {
     taskPriorityElement.classList.remove('text-red-400');
-    taskPriorityElement.classList.remove('test-yellow-400');
+    taskPriorityElement.classList.remove('text-yellow-400');
     taskPriorityElement.classList.add('text-green-400');
   } else if (taskPriority === 'Medium') {
     taskPriorityElement.classList.remove('text-red-400');
@@ -9,7 +9,7 @@ export default function priorityColor(taskPriority, taskPriorityElement) {
     taskPriorityElement.classList.add('text-yellow-400');
   } else if (taskPriority === 'High') {
     taskPriorityElement.classList.remove('text-green-400');
-    taskPriorityElement.classList.remove('test-yellow-400');
+    taskPriorityElement.classList.remove('text-yellow-400');
     taskPriorityElement.classList.add('text-red-400');
   }
 }
