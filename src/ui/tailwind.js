e755fd2423b54129cc175fe9a77df0d523501fd4
Liav <oneandonlyliav@gmail.com>
2022-04-12 17:48:21 -0400

    ported tailwind styles to its own file

--- src/ui/tailwind.js
+++ src/ui/tailwind.js
@@ -1,14 +1,14 @@
-/* eslint-disable import/prefer-default-export */
-export const tailwind = {
+export default {
   /* main display projects */
   projectContainer: 'flex flex-col w-full gap-1',
   projectInfo: 'flex',
   projectName: 'w-full p-1 ml-5 font-bold text-white rounded',
   projectTasks: 'project-tasks flex flex-col w-full gap-1',
-
+  taskAddButton:
+    'w-8 h-6 p-1 rounded hover:bg-slate-100 active:bg-slate-300 fa-solid fa-plus bg-slate-200',
   /* main display tasks */
   taskInfo:
-    'flex items-center gap-5 p-1 rounded cursor-pointer hover:bg-slate-100 active:bg-slate-300 bg-slate-200 task',
+    'flex items-center gap-5 p-1 rounded cursor-pointer hover:bg-slate-100 active:bg-slate-300 bg-slate-200 task-info',
   taskCompleteButton:
     '!hidden w-8 h-6 rounded text-slate-400 task-complete-button bg-slate-700 fa-solid fa-circle-check',
   taskTitle: 'mr-auto text-sm task-title',
@@ -17,7 +17,28 @@ export const tailwind = {
   taskPriority: 'mr-1 text-sm task-priority place-self-center fa-solid fa-flag',
   taskEditButton:
     '!hidden w-8 h-6 p-1 text-sm text-white rounded active:bg-slate-800 hover:bg-slate-600 task-edit-button fa-solid bg-slate-700 fa-pen-to-square',
-
-  taskAddButton:
-    'w-8 h-6 p-1 rounded hover:bg-slate-100 active:bg-slate-300 fa-solid fa-plus bg-slate-200',
+  /* edit task */
+  editTaskPanel:
+    'flex items-center justify-between p-1 text-sm rounded bg-slate-200',
+  editTitle: 'w-40 text-sm rounded max-h-5',
+  editDueDate: 'w-40 text-xs rounded max-h-5',
+  priorityLow:
+    '!hidden w-8 h-6 p-1 text-green-500 rounded priority-selector fa-solid fa-flag bg-slate-700',
+  priorityMed:
+    '!hidden w-8 h-6 p-1 text-yellow-500 rounded priority-selector fa-solid fa-flag bg-slate-700',
+  priorityHigh:
+    '!hidden w-8 h-6 p-1 text-red-500 rounded priority-selector fa-solid fa-flag bg-slate-700',
+  taskEditActionButtons: 'flex gap-0.5',
+  confirmTaskButton:
+    'confirm-task-button w-8 h-6 p-1 text-green-500 rounded active:bg-slate-800 hover:bg-slate-600 bg-slate-700 fa-solid fa-circle-check',
+  deleteTaskButton:
+    'delete-task-button w-8 h-6 p-1 text-red-500 rounded active:bg-slate-800 hover:bg-slate-600 bg-slate-700 fa-solid fa-trash-can',
+  /* edit project */
+  projectEditPanel: 'flex items-center w-full gap-1 p-1 rounded bg-slate-200',
+  editName: 'text-sm rounded max-h-5',
+  projectEditActionButtons: 'flex w-full gap-1',
+  confirmProjectButton:
+    'w-8 h-6 p-1 ml-auto text-green-500 rounded active:bg-slate-800 hover:bg-slate-600 fa-circle-check fa-solid bg-slate-700',
+  deleteProjectButton:
+    'w-8 h-6 p-1 text-red-500 rounded active:bg-slate-800 hover:bg-slate-600 bg-slate-700 fa-trash-can fa-solid',
 };
