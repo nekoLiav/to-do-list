2034435ac496c295d1af4e3001149382947dac59
Liav <oneandonlyliav@gmail.com>
2022-04-12 17:47:46 -0400

    project edit UI now utilizes element factory function

--- src/ui/renderProjectEditUI.js
+++ src/ui/renderProjectEditUI.js
@@ -1,55 +1,33 @@
-import editProject from '../core/editProject';
-import deleteProject from '../core/deleteProject';
+import Element from '../factories/Element';
+import styles from './tailwind';
 
 export default function renderProjectEditUI(projectId) {
-  const sideNavProject = document.querySelectorAll(
+  const sideNavProject = document.querySelector(
     `.side-nav-project[data-id='${projectId}']`
   );
-  const sideNavProjectName = document.querySelectorAll(
+  const sideNavProjectName = document.querySelector(
     `.side-nav-project-name[data-id='${projectId}']`
   );
-  const mainViewProject = document.querySelectorAll(
-    `.project-container[data-id='${projectId}']`
+  // const mainViewProject = document.querySelector(
+  //   `.project-container[data-id='${projectId}']`
+  // );
+
+  const projectEdit = Element(
+    'div',
+    { class: styles.projectEditPanel },
+    Element('input', {
+      class: styles.editName,
+      type: 'text',
+      value: sideNavProjectName.textContent,
+    }),
+    Element(
+      'div',
+      { class: styles.projectEditActionButtons },
+      Element('button', { class: styles.confirmProjectButton }),
+      Element('button', { class: styles.deleteProjectButton })
+    )
   );
 
-  const projectEditPanel = document.createElement('div');
-  const editName = document.createElement('input');
-  const actionButtons = document.createElement('div');
-  const confirmProjectButton = document.createElement('button');
-  const deleteProjectButton = document.createElement('button');
-
-  projectEditPanel.className =
-    'flex items-center w-full gap-1 p-1 rounded bg-slate-200';
-  editName.className = 'text-sm rounded max-h-5';
-  actionButtons.className = 'flex w-full gap-1';
-  confirmProjectButton.className =
-    'w-8 h-6 p-1 ml-auto text-green-500 rounded active:bg-slate-800 hover:bg-slate-600 fa-circle-check fa-solid bg-slate-700';
-  deleteProjectButton.className =
-    'w-8 h-6 p-1 text-red-500 rounded active:bg-slate-800 hover:bg-slate-600 bg-slate-700 fa-trash-can fa-solid';
-
-  editName.type = 'text';
-
-  editName.value = sideNavProjectName[0].textContent;
-
-  confirmProjectButton.addEventListener('click', (e2) => {
-    editProject(projectId, editName.value);
-    e2.target.parentNode.parentNode.remove();
-    sideNavProject[0].classList.remove('hidden');
-  });
-
-  deleteProjectButton.addEventListener('click', (e3) => {
-    deleteProject(projectId);
-    e3.target.parentNode.parentNode.remove();
-    sideNavProject[0].remove();
-    if (mainViewProject[0] !== undefined) {
-      mainViewProject[0].remove();
-    }
-  });
-
-  actionButtons.append(confirmProjectButton, deleteProjectButton);
-  projectEditPanel.append(editName, actionButtons);
-
-  sideNavProject[0].insertAdjacentElement('afterend', projectEditPanel);
-  sideNavProject[0].classList.add('hidden');
-  editName.focus();
+  sideNavProject.insertAdjacentElement('afterend', projectEdit);
+  sideNavProject.classList.add('hidden');
 }
