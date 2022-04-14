5cc6a1ca6930e0799d307e6157edda457273317c
Liav <oneandonlyliav@gmail.com>
2022-04-14 15:57:38 -0400

    removed unnecessary loop from checkComplete

--- src/helpers/checkComplete.js
+++ src/helpers/checkComplete.js
@@ -1,18 +1,22 @@
-export default function checkComplete() {
-  document.querySelectorAll('.task-complete-button').forEach((element) => {
-    const taskId = element.getAttribute('data-task-id');
-    if (element.getAttribute('data-complete') === 'true') {
-      element.classList.remove('text-slate-400');
-      element.classList.add('text-green-500');
-      document
-        .querySelector(`.task-title[data-task-id='${taskId}']`)
-        .classList.add('line-through');
-    } else {
-      document
-        .querySelector(`.task-title[data-task-id='${taskId}']`)
-        .classList.remove('line-through');
-      element.classList.remove('text-green-500');
-      element.classList.add('text-slate-400');
-    }
-  });
+import findTask from './findTask';
+
+export default function checkComplete(projectId, taskId) {
+  const taskTitle = document.querySelector(
+    `.task-title[data-task-id='${taskId}']`
+  );
+  const taskCompleteButton = document.querySelector(
+    `.task-complete-button[data-task-id='${taskId}']`
+  );
+
+  const task = findTask(projectId, taskId);
+
+  if (task.complete) {
+    taskCompleteButton.classList.remove('text-slate-400');
+    taskCompleteButton.classList.add('text-green-500');
+    taskTitle.classList.add('line-through');
+  } else {
+    taskTitle.classList.remove('line-through');
+    taskCompleteButton.classList.remove('text-green-500');
+    taskCompleteButton.classList.add('text-slate-400');
+  }
 }
