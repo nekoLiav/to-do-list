71da3ea77e79e10a5c1f3a01e09a50f0fc59ea8e
Liav <oneandonlyliav@gmail.com>
2022-04-14 19:48:15 -0400

    removed unnecessary function calls from delete modules

--- src/core/deleteProject.js
+++ src/core/deleteProject.js
@@ -1,10 +1,7 @@
 import { toDoList } from './globals';
 import findProject from '../helpers/findProject';
-import updateProject from '../helpers/updateProject';
 
 export default function deleteProject(projectId) {
   const project = findProject(projectId);
   toDoList.splice(toDoList.indexOf(project), 1);
-
-  updateProject();
 }
--- src/core/deleteTask.js
+++ src/core/deleteTask.js
@@ -1,12 +1,9 @@
 import findProject from '../helpers/findProject';
 import findTask from '../helpers/findTask';
-import updateTask from '../helpers/updateTask';
 
 export default function deleteTask(projectId, taskId) {
   const project = findProject(projectId);
   const task = findTask(projectId, taskId);
 
   project.tasks.splice(project.tasks.indexOf(task), 1);
-
-  updateTask(project, task);
 }
